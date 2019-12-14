92b37394c0102ebf618ed7b190d919f93a1e9216
DAN-BIWOTT <dankibiwott254@gmail.com>
2019-12-15 02:24:16 +0300

    home

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//
+//= require jquery
+//= require scroll-desktop
+//= require scroll-desktop-smooth
+//= require jquery.themepunch.revolution.min
+//= require jquery.themepunch.tools.min
+//= require parallax
+//= require countdown
+//= require owl.carousel
+//= require cart-detail
+//= require jquery.mmenu.min.all
+//= require form-dropdown
+//= require classie
+//= require jquery-ui-1.10.3.custom
+//
+//= require custom
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
