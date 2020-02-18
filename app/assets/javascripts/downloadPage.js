c3d4f1dcfae3c0d11a008c5512e92392df264638
DAN-BIWOTT <dankibiwott254@gmail.com>
2020-02-18 14:48:32 +0300

    finished the download page

--- app/assets/javascripts/downloadPage.js
+++ app/assets/javascripts/downloadPage.js
@@ -1,10 +1,10 @@
 document.addEventListener("turbolinks:load", function () {
-    // $('.owl-carousel').owlCarousel({
-    //     loop: true,
-    //     margin: 10,
-    //     responsiveClass: false,
-    //     items: 1,
-    //     center: true
-    // });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: false,
+        items: 1,
+        center: true
+    });
 
 })
