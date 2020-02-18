faf7fe8e3c5d6e635229b6c2204d4776114ae2fc
DAN-BIWOTT <dankibiwott254@gmail.com>
2020-02-18 15:39:06 +0300

    finished the download page

--- app/assets/javascripts/downloadPage.js
+++ app/assets/javascripts/downloadPage.js
@@ -1,4 +1,18 @@
 document.addEventListener("turbolinks:load", function () {
-
-
+    console.log("dowload page reached")
+    $("#btn-widgets").click(function () {
+        $('#admin-panels').fadeOut('slow');
+        $('#how-to-use').fadeOut('slow');
+        $('#widgets').fadeIn(2000);
+    })
+    $("#btn-admin-panels").click(function () {
+        $('#widgets').fadeOut('slow');
+        $('#how-to-use').fadeOut('slow');
+        $('#admin-panels').fadeIn(2000);
+    })
+    $("#btn-help").click(function () {
+        $('#widgets').fadeOut('slow');
+        $('#admin-panels').fadeOut('slow');
+        $('#how-to-use').fadeIn(2000);
+    })
 })
